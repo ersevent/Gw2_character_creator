b77a04e13970be9df05ce72912a0bf0a50e343da
ersevent <ersevent@gmail.com>
2021-04-20 13:40:15 +0200

    heartbeat

--- frontend/src/components/InitialPage.js
+++ frontend/src/components/InitialPage.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import useUser from "../hooks/useUser"
 import { useHistory } from "react-router-dom"
+import axios from "axios";
 
 
 const InitialPage = () => {
     const { successfulLogin } = useUser();
     const history = useHistory();
+    const [heartbeat, setHeartbeat] = useState("")
 
     useEffect(() => {
         const goToHomePage = () => {
@@ -17,11 +19,20 @@ const InitialPage = () => {
         // eslint-disable-next-line
     }, [successfulLogin])
 
+    useEffect(() => {
+        const tryHeartbeat = async () => {
+            const { data } = await axios.get('/api/heartbeat')
+            setHeartbeat(data)
+        }
+        tryHeartbeat()
+    }, [])
+
     //TODO userlogin check
 
     return (
         <form>
             <h1>Need to log in</h1>
+            {JSON.stringify(heartbeat)}
         </form>
     )
 }
